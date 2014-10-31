93511f26ba398234a03b71345c40fa18432869dc
Nathan Valentine <nathan@puppetlabs.com>
2014-10-31 13:12:22 -0700

    allow jenkins in inject firewall rules

--- manifests/profile/jenkins/master.pp
+++ manifests/profile/jenkins/master.pp
@@ -3,5 +3,5 @@ class site::profile::jenkins::master inherits ::site::params {
 
   # puppetlabs/firewall appears to be broken for CentOS7/systemd right now
   class { '::firewall': ensure => stopped, }
-  class { '::jenkins': } #configure_firewall => true, }
+  class { '::jenkins': configure_firewall => true, }
 }
