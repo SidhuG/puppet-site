8ecfac13d3bd965e1609c08ad3b8f4a29f46a03d
Nathan Valentine <nathan@puppetlabs.com>
2014-11-03 18:43:07 -0800

    allow pass of jenkins plugins as a hash

--- manifests/profile/jenkins/master.pp
+++ manifests/profile/jenkins/master.pp
@@ -1,7 +1,16 @@
-class site::profile::jenkins::master inherits ::site::params {
+class site::profile::jenkins::master(
+  $plugins = {}
+) inherits ::site::params {
   require ::site::profile::jenkins
+
+  validate_hash($plugins)
 
   # puppetlabs/firewall appears to be broken for CentOS7/systemd right now
   class { '::firewall': ensure => stopped, }
   class { '::jenkins': configure_firewall => true, }
+
+  include ::git
+
+  $jenkins_plugins_defaults = {}
+  create_resources('jenkins::plugin', $plugins, $jenkins_plugins_defaults)
 }
