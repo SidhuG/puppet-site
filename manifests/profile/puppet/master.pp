f8be6f18597720f182903e6c456e5bd7f1eec508
Nathan R Valentine <nrvale0@gmail.com>
2015-06-15 20:08:37 -0700

    getting closer to a first pass

--- manifests/profile/puppet/master.pp
+++ manifests/profile/puppet/master.pp
@@ -7,4 +7,19 @@ class site::profile::puppet::master(
   require ::site::profile::base
 
   unless false == $enable_r10k { require ::r10k }
+
+  $environments_dir = '/etc/puppetlabs/puppet/environments'
+  file { $environments_dir:
+    ensure => directory,
+    mode => 0755,
+    owner => 'root',
+    group => 'root',
+  }
+
+  file { 'hiera router config':
+    ensure => link,
+    path => '/etc/puppetlabs/puppet/hiera.yaml',
+    target => '/etc/puppetlabs/puppet/environments/production',
+    notify => Service['pe-puppetmaster'],
+  }
 }
